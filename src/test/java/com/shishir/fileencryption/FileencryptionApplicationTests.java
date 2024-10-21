ebdbfbde2cc3d4ab1195c29bcd074dedaa803e3d
Shishir Karki <shishir4913120@gmail.com>
2024-10-21 19:04:55 +0545

    FEAT initialize spring boot

---
+++ src/test/java/com/shishir/fileencryption/FileencryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shishir.fileencryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileencryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
